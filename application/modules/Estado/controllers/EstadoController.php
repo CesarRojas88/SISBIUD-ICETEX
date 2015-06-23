f57b7390c2a58555488f80c00aeedf9683775970
Cesar <hohner.rojas@gmail.com>
2015-06-23 02:50:43 -0500

    Modulo EStado

--- application/modules/Estado/controllers/EstadoController.php
+++ application/modules/Estado/controllers/EstadoController.php
@@ -15,8 +15,20 @@ class Estado_EstadoController extends Zend_Controller_Action
         // action body
         if (Zend_Auth::getInstance()->hasIdentity()) 
         {
-            //$formulario = new Estado_Form_Estado();
-            //$this->view->anadir=$formulario;
+            $listaEstado = new Estado_Model_DbTable_EstadoDeCredito();
+            $this->view->listaEstado = $listaEstado->Estados();
+            $formulario = new Estado_Form_Estado();
+            $this->view->anadir=$formulario;
+            if ($this->getRequest()->isPost()) {
+                $campos = $this->getRequest()->getPost();
+                if ($formulario->isValid($campos)) {
+                    $tipo = $formulario->getValue ('tipo');
+                    $descripcion = $formulario->getValue('descripcion');
+                    $insertar = new Estado_Model_DbTable_EstadoDeCredito();
+                    $insertar->insertarEstado($tipo, $descripcion);
+                    $this->_helper->redirector('index');
+                }
+            }
         } else 
         {
             $this->_redirect('Index/index');
--- application/modules/Estado/forms/Estado.php
+++ application/modules/Estado/forms/Estado.php
@@ -6,16 +6,21 @@ class Estado_Form_Estado extends Zend_Form
     public function init()
     {
         /* Form Elements & Other Definitions Here ... */
-        $this->setName('Estado');
+        $this->setName('estado');
         $tipo = new Zend_Form_Element_Text('tipo');
         $descripcion = new Zend_Form_Element_Text('descripcion');
         $submit = new Zend_Form_Element_Submit('insertar');
-        $submit->setLabel('Insertar');
+        $submit->setLabel('Insertar')
+               ->setAttrib('class', 'btn btn-primary');
         $tipo->setAttrib('required', 'required')
              ->setRequired(true)
-             ->setValidators('noEmpty');
+             ->setAttrib('class', 'form-control')
+             ->addValidator('notEmpty');
+        $descripcion->setAttrib('required', 'required')
+                    ->addValidator('notEmpty')
+                    ->setAttrib('class', 'form-control');
         $this->addElements(array($tipo,$descripcion,$submit));
-        $this->setElementDecorators(array('ViewHelper',"Errors")) ;
+        $this->setElementDecorators(array('ViewHelper',"Errors"));
     }
 
 
