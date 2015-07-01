8c051d71be7fa190e51e84566cc9c639a6d9cbd3
lvcc <cobraliz@hotmail.com>
2015-07-01 16:01:06 -0500

    Cambios para actualizar usuario, pero no actualiza por un error

--- application/modules/Usuarios/controllers/UsuarioController.php
+++ application/modules/Usuarios/controllers/UsuarioController.php
@@ -63,7 +63,70 @@ class Usuarios_UsuarioController extends Zend_Controller_Action
         }
     }
 
+    public function actualizarusuarioAction()
+    {
+        if (Zend_Auth::getInstance()->hasIdentity()) 
+        {
+            $this->view->title="Actualizar Usuario - ";
+            $this->view->headTitle($this->view->title);
+
+            $formEditar=new Usuarios_Form_Usuarios();
+            $this->view->form=$formEditar;
+            //var_dump($formEditar);
+            if($this->getRequest()->isPost())
+            {
+                $formDatos=  $this->getRequest()->getPost();
+                //var_dump($formDatos);
+                if($formEditar->isValid($formDatos))
+                {
+                    $nombreusuario=$formEditar->getValue('nombre_usuario');
+                    $contrasena=$formEditar->getValue('contrasena');
+                    $id=$formEditar->getValue('identificacion');
+                    $nombres=$formEditar->getValue('nombre_real');
+                    $apellidos=$formEditar->getValue('apellido_real');
+                    $cargo=$formEditar->getValue('cargo');
+                    $perfil=$formEditar->getValue('perfil');
+                    $estado=$formEditar->getValue('estado');
+                    
+                    $formActualizar=new Usuarios_Model_DbTable_Usuario();
+                    var_dump($formActualizar->modificarUsuario($nombreusuario, $contrasena, $id, $nombres, $apellidos, $cargo, $perfil, $estado));
+                    /*
+                    $this->_helper->redirector('index');*/
+                }
+                else
+                {
+                    echo 'ononono';
+                    //$formEditar->populate($formDatos);
+                }
+            }
+            else
+            {
+                
+                $id=$this->_getParam('id',0);
+                if(strlen($id)>0)
+                {
+                    $formActualizar=new Usuarios_Model_DbTable_Usuario();
+                    $actualizar=$formActualizar->get($id);
+                    $formEditar->populate($actualizar);
+                    
+                }
+            }
+        }else 
+        {
+            $this->_redirect('Index/index');
+        }
+    }
+
+    public function eliminarusuarioAction()
+    {
+        // action body
+    }
+
 
 }
+
+
+
+
 
 
